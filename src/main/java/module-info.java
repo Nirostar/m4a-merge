5c4b25028a5c8b29c9e162f8c6620252f1bb184f
Niklas Roth <e1425096@student.tuwien.ac.at>
2021-05-13 19:25:58 +0200

    Fix everything now msi installer works

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module m4a_merge {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.media;
+    requires java.logging;
+    requires java.desktop;
+
+    requires jcodec;
+    requires org.apache.commons.text;
+    requires org.apache.commons.lang3;
+    requires jaudiotagger;
+
+    opens at.nirostar_labs.m4a_merge.ui to javafx.fxml;
+    opens at.nirostar_labs.m4a_merge.model to javafx.base;
+
+    exports at.nirostar_labs.m4a_merge;
+}
